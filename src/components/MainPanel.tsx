b537e141c6180e86dc14f664805b6da4cd513685
xchrl <michalbronicki85@gmail.com>
2022-10-13 01:00:13 +0200

    Changed conditional rendering on SearchBar component from basic if to ternary

--- src/components/MainPanel.tsx
+++ src/components/MainPanel.tsx
@@ -1,11 +1,12 @@
 export default function MainPanel({ fetchedData }: any) {
-  if (fetchedData === null) return <></>;
-  return (
+  return fetchedData !== null ? (
     <>
       <h1>City: {fetchedData.name}</h1>
       <h3>Lat: {fetchedData.weatherData.lat}</h3>
       <h3>Lon: {fetchedData.weatherData.lon}</h3>
       <h3>Temperature: {fetchedData.weatherData.current.temp} °C</h3>
     </>
+  ) : (
+    <></>
   );
 }
