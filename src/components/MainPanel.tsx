67155f01fcaeeb3217fa17110f7db009e94ed311
xchrl <michalbronicki85@gmail.com>
2022-10-12 19:42:31 +0200

    Implemented waiting for data load in a better way (using null). Basically the same as last commit but better.

--- src/components/MainPanel.tsx
+++ src/components/MainPanel.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import fetchWeatherData from "../hooks/fetchWeatherData";
 
 export default function MainPanel() {
-  const [data, setData] = useState<any>({});
+  const [data, setData] = useState<any>(null);
 
   useEffect(() => {
     fetchWeatherData("Przemysl").then((res) => setData(res));
@@ -19,15 +19,13 @@ export default function MainPanel() {
 
   // Method above uses the .map function to map specific object values to elements.
 
+  if (data === null) return <h1>Loading data...</h1>;
   return (
-    data &&
-    data.weatherData && (
-      <>
-        <h1>City: {data.name}</h1>
-        <h3>Lat: {data.weatherData.lat}</h3>
-        <h3>Lon: {data.weatherData.lon}</h3>
-        <h3>Temperature: {data.weatherData.current.temp} °C</h3>
-      </>
-    )
+    <>
+      <h1>City: {data.name}</h1>
+      <h3>Lat: {data.weatherData.lat}</h3>
+      <h3>Lon: {data.weatherData.lon}</h3>
+      <h3>Temperature: {data.weatherData.current.temp} °C</h3>
+    </>
   );
 }
