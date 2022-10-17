022308d3e1e8b8d038ff65b5f54accde500e96f6
xchrl <michalbronicki85@gmail.com>
2022-10-17 19:59:02 +0200

    Corrected the displayNormalizedTime examples for better clarity

--- src/hooks/displayNormalizedTime.ts
+++ src/hooks/displayNormalizedTime.ts
@@ -1,15 +1,15 @@
 export default function displayNormalizedTime(hours: number, minutes: number) {
   if (hours >= 0 && hours <= 9 && minutes >= 0 && minutes <= 9) {
     return `0${hours}:0${minutes}`;
-    // 09:04
+    // 9:4 -> 09:04
   } else if (hours >= 10 && minutes >= 0 && minutes <= 9) {
     return `${hours}:0${minutes}`;
-    // 10:04
+    // 10:4 -> 10:04
   } else if (hours >= 0 && hours <= 9 && minutes >= 10) {
     return `0${hours}:${minutes}`;
-    // 09:14
+    // 9:14 -> 09:14
   } else {
     return `${hours}:${minutes}`;
-    // 10:14
+    // 10:14 -> 10:14
   }
 }
