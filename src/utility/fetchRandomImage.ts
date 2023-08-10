e1e196d6e14dc3538263533e2660741a65f1e083
xchrl <michalbronicki85@gmail.com>
2023-08-10 17:28:50 +0200

    fix(images): handled no image being found

--- src/utility/fetchRandomImage.ts
+++ src/utility/fetchRandomImage.ts
@@ -2,10 +2,14 @@ export default async function fetchRandomImage(query: string) {
   query.split(" ").length != 1 ? (query = query.split(" ").join(",")) : query;
 
   const data = await fetch(
-    `https://api.unsplash.com/search/photos?query=${query}&page=1&per_page=1&client_id=${import.meta.env.VITE_UNSPLASH_CLIENT_ID}`
+    `https://api.unsplash.com/search/photos?query=${query}&page=1&per_page=1&client_id=${
+      import.meta.env.VITE_UNSPLASH_CLIENT_ID
+    }`
   )
     .then((resp) => resp.json())
     .catch((err) => console.log(err));
 
-    return data.results[0].urls.full;
+  return data.results.length != 0
+    ? data.results[0].urls.full
+    : console.error("No picture found!");
 }
